5dbbadb655906600263e5cbee20b62dd768a718b
remal <remal@DNS>
2013-05-25 03:41:27 +0400

    ajax load

--- js/main.js
+++ js/main.js
@@ -2,3 +2,21 @@ function HelloWorld()
 {
 	alert("HelloWorld");
 }
+
+function LoadJSON()
+{
+	var xmlhttp;
+	if (window.XMLHttpRequest)
+	{
+		xmlhttp = new XMLHttpRequest();
+	}
+	xmlhttp.onreadystatechange = function()
+	{
+		if (xmlhttp.readyState === 4 && xmlhttp.status === 200)
+		{
+			document.getElementById("fpscontainer").innerHTML = xmlhttp.responseText;
+		}
+	}
+	xmlhttp.open("GET", "json/simple.json", true);
+	xmlhttp.send();
+}
